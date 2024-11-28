0736a7e4a61b7d46133be636c04396973e57a8da
jhyrus <diego.morales.medrano@gmail.com>
2024-11-28 18:48:45 -0500

    Ajustes 2

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import joblib
 import numpy as np
 import pandas as pd
+import base64
 
 # Cargar el modelo
 model = joblib.load("decision_tree_pipeline.pkl")
@@ -15,8 +16,39 @@ relevant_columns = [
 # Opciones para Campaign type
 campaign_types = ['BRANDING', 'PERFORMANCE', 'TACTICOS', 'MARKETING CLOUD']
 
+# Función para cargar imágenes como base64
+def get_base64_image(image_path):
+    with open(image_path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode()
+
+# Cargar las imágenes locales en base64
+honda_logo = get_base64_image("honda.png")
+vml_logo = get_base64_image("vml.png")
+
+# Crear la cabecera con HTML y CSS
+header_html = f"""
+<div style="
+    background-color: #c8102e;
+    padding: 10px;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+">
+    <div style="color: white; font-size: 24px; font-weight: bold;">
+        Predicción de Éxito en Campañas Publicitarias
+    </div>
+    <div style="display: flex; gap: 20px;"> <!-- Contenedor para logos alineados lado a lado -->
+        <img src="data:image/png;base64,{honda_logo}" alt="Honda Logo" style="height: 50px;">
+        <img src="data:image/png;base64,{vml_logo}" alt="VML Logo" style="height: 50px;">
+    </div>
+</div>
+"""
+
+# Mostrar la cabecera en la app
+st.markdown(header_html, unsafe_allow_html=True)
 # Interfaz de usuario
-st.title("Predicción de Éxito en Campañas Publicitarias")
+# st.title("Predicción de Éxito en Campañas Publicitarias")
 
 # Crear inputs para las variables
 st.subheader("Por favor, ingresa los valores de las siguientes variables:")
